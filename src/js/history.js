d63ad1ed54b65ca1d75b49a7ee95c85b9e51f98a
tobisns <16521345@std.stei.itb.ac.id>
2022-08-14 15:20:32 +0700

    reconstruct history

--- src/js/history.js
+++ src/js/history.js
@@ -1,15 +1,49 @@
+let thisMonth = {
+    "StartingBalance" : 1000000,
+    "InFlow" : 15000000,
+    "OutFlow" : 12500000
+};
+
+let lastMonth = {
+    "StartingBalance" : 2000000,
+    "InFlow" : 17000000,
+    "OutFlow" : 12500000
+};
+
+let future = {
+    "StartingBalance" : 5000000,
+    "InFlow" : 5000000,
+    "OutFlow" : 12500000
+};
+
 let btn = document.querySelector("#btn1");
 let btn2 = document.querySelector("#btn2");
 let btn3 = document.querySelector("#btn3");
 let lastbtn = btn2;
-let iref = document.querySelector("iframe");
+let sB = document.querySelector("#stBal");
+let eB = document.querySelector("#enBal");
+let infw = document.querySelector("#inFlw");
+let outfw = document.querySelector("#outFlw");
+let net = document.querySelector("#net");
+
+sB.textContent = ("Rp"+thisMonth.StartingBalance.toString());
+infw.textContent = ("Rp"+thisMonth.InFlow.toString());
+outfw.textContent = ("Rp"+thisMonth.OutFlow.toString());
+net.textContent = ("Rp"+(thisMonth.InFlow-thisMonth.OutFlow).toString());
+eB.textContent = ("Rp"+(thisMonth.StartingBalance+(thisMonth.InFlow-thisMonth.OutFlow)).toString())
+
 
 btn.addEventListener("click", ()=>{
 
     btn.classList.toggle("clicked");
     lastbtn.classList.toggle("clicked");
-    lastbtn = btn
-    iref.src = "/src/historyIFrame/lastMonth.html"
+    lastbtn = btn;
+    sB.textContent = ("Rp"+lastMonth.StartingBalance.toString());
+    infw.textContent = ("Rp"+lastMonth.InFlow.toString());
+    outfw.textContent = ("Rp"+lastMonth.OutFlow.toString());
+    net.textContent = ("Rp"+(lastMonth.InFlow-lastMonth.OutFlow).toString());
+    eB.textContent = ("Rp"+(lastMonth.StartingBalance+(lastMonth.InFlow-lastMonth.OutFlow)).toString())
+
 }, false);
 
 btn2.addEventListener("click", ()=>{
@@ -17,7 +51,11 @@ btn2.addEventListener("click", ()=>{
     btn2.classList.toggle("clicked");
     lastbtn.classList.toggle("clicked");
     lastbtn = btn2
-    iref.src = "/src/historyIFrame/thisMonth.html"
+    sB.textContent = ("Rp"+thisMonth.StartingBalance.toString());
+    infw.textContent = ("Rp"+thisMonth.InFlow.toString());
+    outfw.textContent = ("Rp"+thisMonth.OutFlow.toString());
+    net.textContent = ("Rp"+(thisMonth.InFlow-thisMonth.OutFlow).toString());
+    eB.textContent = ("Rp"+(thisMonth.StartingBalance+(thisMonth.InFlow-thisMonth.OutFlow)).toString())
 }, false);
 
 btn3.addEventListener("click", ()=>{
@@ -25,5 +63,9 @@ btn3.addEventListener("click", ()=>{
     btn3.classList.toggle("clicked");
     lastbtn.classList.toggle("clicked");
     lastbtn = btn3
-    iref.src = "/src/historyIFrame/future.html"
+    sB.textContent = ("Rp"+future.StartingBalance.toString());
+    infw.textContent = ("Rp"+future.InFlow.toString());
+    outfw.textContent = ("Rp"+future.OutFlow.toString());
+    net.textContent = ("Rp"+(future.InFlow-future.OutFlow).toString());
+    eB.textContent = ("Rp"+(future.StartingBalance+(future.InFlow-future.OutFlow)).toString())
 }, false);
